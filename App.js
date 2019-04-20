b76fc390e62662d38e2ed185c85490bf89afca92
Sabrina Tee <sabrinatee@me.com>
2019-04-20 17:03:04 +0800

    Init simple to do app

--- App.js
+++ App.js
@@ -1,49 +1,86 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react'
+import { ScrollView } from 'react-native'
+import { ThemeProvider, Card, Input, CheckBox, Button } from 'react-native-elements'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      list: [],
+      textInput: ''
+    }
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  addItem () {
+    if (this.state.textInput.trim().length !== 0) {
+      // Creating new task item
+      let newTask = {title: this.state.textInput, completed: false}
+      // Updating task list
+      let newTaskList = this.state.list
+      newTaskList.push(newTask)
+      // Updating state with new task list
+      this.setState({
+        list: newTaskList,
+        textInput: ''
+      })
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+  updateItem (index, itemStatus) {
+    // Updating completed status of task item
+    let updatedTaskItem = {
+      ...this.state.list[index],
+      completed: !itemStatus
+    }
+    // Updating task list with updated task item
+    let newTaskList = this.state.list
+    newTaskList.splice(index, 1, updatedTaskItem)
+    // Updating state with updated task list
+    this.setState({
+      list: newTaskList
+    })
+  }
+
+  render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      <ThemeProvider>
+        <ScrollView>
+          <Card
+            title='Welcome to GAB 2019!'
+            image={require('./resources/gab_logo.png')}
+            imageProps={{resizeMode: 'contain'}} />
+          <Card>
+            <Input
+              label={'Add a to-do item'}
+              value={this.state.textInput}
+              placeholder={'+'}
+              onChangeText={(textInput) => this.setState({textInput})}
+              onSubmitEditing={() => this.addItem()}
+              returnKeyType={'done'}
+            />
+            <Button
+              title='Add'
+              type='outline'
+              raised
+              containerStyle={{marginTop: 20}}
+              onPress={() => this.addItem()}
+            />
+          </Card>
+          {
+            this.state.list.length !== 0
+              ? <Card>
+                {
+                  this.state.list.map((item, index) => <CheckBox
+                    key={index}
+                    title={item.title}
+                    checked={item.completed}
+                    onPress={() => this.updateItem(index, item.completed)} />)
+                }
+              </Card>
+              : null
+          }
+        </ScrollView>
+      </ThemeProvider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
